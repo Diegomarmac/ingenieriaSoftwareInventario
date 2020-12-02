87c941a5942dc654a025fde3686c120d40dbf70f
Diego <diego.martinez.ivan@gmail.com>
2020-12-02 16:54:37 -0600

    faker snippet

--- fake-data/snippet.js
+++ fake-data/snippet.js
@@ -3,10 +3,10 @@ const faker = require('faker');
 n = 0
 
 while(n <  60){
-    let producto = ("Moldura-" + faker.commerce.product()+"-"+ faker.commerce.color() );
+    let producto = ("Moldura-" + faker.commerce.productMaterial()+"-" + faker.commerce.color() );
     let precio   = faker.commerce.price(150, 500,0, "$");
     let sku      = ("SKU-" + faker.random.alphaNumeric(8) );
-    let cantidad = faker.random.number();
-    console.log(sku + " "+ producto+" " + precio+ " " + cantidad);
+    let cantidad = faker.random.number({min:0, max: 500 });
+    console.log(sku + " "+ producto+" " + precio+ " " + "cantidad:"+  cantidad);
     n ++;
 }
